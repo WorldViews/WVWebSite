a79d8c61bd676d0ab68f0b707f2c1a4b5923d523
donkimber <donkimber@gmail.com>
2021-03-28 15:41:07 -0700

    support using different branches for project DB

--- js/Project.js
+++ js/Project.js
@@ -1,49 +1,5 @@
 "use strict";
 
-var PROJECTS = {
-    "projects": [
-        {
-            "name": "The Garden",
-            "description": "The Garden was created as a way to bring people and organizations together to work, play and dance. Some of its activities are in the form of service or creative projects, some are classes or workshops, some are performances, and some are open space mixers with no agenda.",
-            "image": "community.jpg"
-        },
-        {
-            "name": "WorldViews Organization and Community",
-            "description": "The mission of this project is to create and nurture a vibrant community of people working on WorldViews, enjoying the project, or using it to share views of the world.",
-            "image": "virtualtour.jpg"
-        },
-        {
-            "name": "Guide and Expert Registry",
-            "description": "This project will make it easy to find people around any location who can tell you about that location and provide views or tours of the area. This works by listing current live streams or guides ready to stream using Periscope, Meerkat, or other mobile streaming technologies. We also have a ShareCamApp powered by the JumpCh.at WebRTC system that lets people take pictures together. The Registry lists active guides, or guides willing to provide views for upcoming events, and for others to list views or event coverage they would like to see.",
-            "image": "techdevelopment.jpg"
-        },
-        {
-            "name": "WorldViews Technology Development",
-            "description": "This activity is to build cool server and client software for furthering the WorldViews mission. All our software is open source, and built on open source. Currently the main efforts are building a client virtual earth browser called TeleViewer, which can show many layers of information, videos, and live video feeds. TeleViewer is built upon the open source CesiumJS. Another part of the current development is to develop a server to support many WorldViews capabilities. We currently support serving the TeleViewer using either nodejs, django, python flask, or a simple python server. We primarily are focusing on flask, and use the mongodb.",
-            "image": "worldviews.png"
-        },
-        {
-            "image": "toyokawa.jpg",
-            "name": "TeleViewer",
-            "description": "Our first  project was TeleViewer, which is a digital earth supporting many different ways to see the world or any part of the world. These may be based on images, video, computer graphics, or live streamed views, telepresence robotic views, etc. It is inspired by Google Eath, StreetView and many great related projects, but we seek to build open source tools with core content in the creative commons. Our ultimate vision is to have free tools available to anyone to let them see the world or any part of the world in useful and inspiring ways."
-        },
-        {
-            "image": "toyokawa.jpg",
-            "name": "Cupertino - Toyokawa Sister City Program",
-            "description": "WorldViews is interested in ways to help connect people and share experiences across sister cities. We are working with the Cupertino Toyokawa Sister Cities program to explore ways to do this. Our first effort at this will be during the Cupertino Cherry Blossom Festival."
-        },
-        {
-            "image": "polly-med.jpg",
-            "name": "Next Generation Polly",
-            "description": "Polly was an FXPAL project, based on the idea of a 'parrot on the shoulder' serving as an avatar for someone wanting to virtualy visit a location they can't physically access. It is a bit like using a telepresence robot, but instead of the person connecting to a robot, they connect to the robotic parrot being carried by a guide. It was fun using Polly to take people on virtual tours, and was especially rewarding to take Henry and Jane Evans on a tour of the Stanford Business school, where Henry had received his MBA (shown in video above.) As is clearly seen, our prototype, designed and 3D printed by Don Severns worked, but was rather clunky and fragile. We always hoped for a Polly type device to become more available. Now DJI has put out a product called the Osmo mobile, which is very much like Polly. We are planning to use this to revive the Polly idea, and give more virtual tours. Enock has made good progress on this, getting the DJI sample code to work for controlling the gimbal."
-        },
-        {
-            "image": "horseshoepond.jpg",
-            "name": "Drone Views",
-            "description": "Inspired by the sites Travel By Drone and Travel With Drone we would like to have a layer that shows beautiful videos captured by drones from all around the world. We would also like to show current live drone videos, and allow users to make a request to have videos shot at various locations of their interest. We will also work to provide software so that drone flights can be remotely controlled (within geobox and safety control guidelines) by remote people watching on WorldViews."
-        }
-    ]
-};
 
 var numProjs = 0;
 function getDiv(container) {
@@ -59,10 +15,14 @@ function getDiv(container) {
 
 class ProjectDB {
     constructor(opts) {
-        this.opts = opts || {};
+        opts = opts || {};
+        this.opts = opts;
         this.projectsObj = null;
         this.projectsById = {};
         this.allowEdits = false;
+        var dbRoot = getParameterByName("dbRoot");
+        this.dbRoot = dbRoot || opts.dbRoot || "/topics";
+
         var inst = this;
         this.setupGUI();
         this.initFirebase();
@@ -140,28 +100,17 @@ class ProjectDB {
         if (this.firebaseDB)
             return this.firebaseDB;
         this.initFirebase();
-        /*
-        var firebaseConfig = {
-            apiKey: "AIzaSyBqAsqHaBZGT-UsC82ShV3koGWWgu-l8to",
-            authDomain: "fir-helloworld-39759.firebaseapp.com",
-            databaseURL: "https://fir-helloworld-39759.firebaseio.com",
-            projectId: "fir-helloworld-39759",
-            storageBucket: "fir-helloworld-39759.appspot.com",
-            messagingSenderId: "1080893233748",
-            appId: "1:1080893233748:web:1614aab0d167c094322bc1"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
-        */
         this.firebaseDB = this.firebase.database();
         console.log("db:", this.firebaseDB);
         return this.firebaseDB;
     }
 
     loadProjectsFromFirebase() {
+        console.log("loadProjectsFromFirebase root:", this.dbRoot);
         var db = this.getFirebaseDB();
         console.log("db:", db);
-        var dbRef = db.ref('/topics');
+        //var dbRef = db.ref('/topics');
+        var dbRef = db.ref(this.dbRoot);
         console.log("Got dbRef", dbRef);
         return new Promise((res, rej) => {
             dbRef.on('value', snap => {
@@ -288,9 +237,14 @@ class ProjectDB {
         var db = this.getFirebaseDB();
         console.log("db:", db);
         //var dbRef = db.ref('/text');
+        /*
         var dbRef = db.ref();
         console.log("Got dbRef", dbRef);
         await dbRef.child("topics").set(this.projectsObj);
+        */
+        var dbRef = db.ref(this.dbRoot);
+        console.log("dbRef", dbRef);
+        await dbRef.set(this.projectsObj);
         console.log("Successfully updated");
     }
 
@@ -307,7 +261,7 @@ class ProjectDB {
             inst.updateProject();
         });
         $("#cancelButton").click(() => {
-            inst.launchProjectList();
+            inst.launchNextPage();
         });
         $("#deleteButton").click(() => {
             inst.deleteProject(inst.currentProj);
@@ -331,14 +285,13 @@ class ProjectDB {
             alert("cannot find project to delete");
         }
         await this.updateDB();
-        this.launchProjectList();
+        this.launchNextPage();
     }
 
 
     async layoutProjects(opts) {
         opts = opts || {};
         console.log("layoutProjects", opts);
-        //projects = PROJECTS.projects;
         var inst = this;
         var projects = await this.getProjects(opts);
         projects.forEach(project => {
@@ -380,7 +333,7 @@ class ProjectDB {
         }
         this.checkId(proj);
         await this.updateDB();
-        this.launchProjectList();
+        this.launchNextPage();
     }
 
     launchCreateProject() {
@@ -391,8 +344,11 @@ class ProjectDB {
         window.open(url, "_self");
     }
 
-    launchProjectList() {
-        var url = "projectList.html?edit=True";
+    launchNextPage() {
+        var nextPage = getParameterByName("nextPage");
+        var url = nextPage || this.nextPage || "projectList.html";
+        url += "?edit=True";
+        url += "&dbRoot="+this.dbRoot;
         if (username)
             url = url + "&username=" + username;
         window.open(url, "_self");
@@ -400,10 +356,14 @@ class ProjectDB {
 
     // called to change to a new webpage for editing a project
     launchEditProject(projId) {
-        var url = "projectEdit.html?projectId=" + projId;
+        var url = sprintf("projectEdit.html?projectId=%s&dbRoot=%s", projId, this.dbRoot);
+        if (this.nextPage)
+            url += "&nextPage="+this.nextPage;
         if (username)
             url = url + "&username=" + username;
-        window.open(url, "_self");
+        var winName = this.editWindowName || "_self";
+        console.log("winName", winName);
+        window.open(url, winName);
     }
 
     // called to set up things for editing project on this webpage
